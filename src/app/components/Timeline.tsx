b9fb64d377bfdfdaa575d501e2f1d1c916dbcb07
realeselvis7 <elvisreales7@gmail.com>
2025-09-25 19:54:33 +0000

    Estructura de timeline lista, pendientes props y estilos

---
+++ src/app/components/Timeline.tsx
@@ -0,0 +1,45 @@
+"use client";
+import React from "react";
+
+type TimelineEvent = {
+  title: string;
+  description: string;
+  icon?: React.ReactNode; 
+};
+
+type Props = {
+  events: TimelineEvent[];
+};
+
+const Timeline: React.FC<Props> = ({ events }) => {
+  return (
+    <ol className="relative border-s-4 border-[#1Ddad2]">
+      {events.map((event, index) => (
+        <li key={index} className="mb-10 ms-6">
+          <span className="absolute flex items-center justify-center w-8 h-8 rounded-full -start-4 bg-white ring-8 ring-[#1C2A29]">
+            {event.icon ?? (
+              <svg
+                className="w-2.5 h-2.5 text-blue-900"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+              >
+                <circle cx="10" cy="10" r="10" />
+              </svg>
+            )}
+          </span>
+	        <div className="p-4 bg-[#00514B] rounded-lg">
+            <div className="items-center justify-between mb-3 sm:flex">
+              <time className="mb-1 text-xs font-normal text-gray-400 sm:order-last sm:mb-0">2 hours ago</time>
+              <div className="text-sm font-normal text-gray-200 lex dark:text-gray-300">Thomas Lean commented on  <a href="#" className="font-semibold text-gray-600 dark:text-white hover:underline">{event.title}</a></div>
+            </div>
+				    <div className="p-3 rounded-lg bg-[#1C2A29]">
+              <p className=" text-xs italic font-normal text-gray-200">{event.description}</p>
+            </div>
+		        </div>
+        </li>
+      ))}
+    </ol>
+  );
+};
+
+export default Timeline;
