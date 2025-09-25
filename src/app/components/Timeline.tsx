b60872a26e6e379116bccf398faa34b753f2dade
realeselvis7 <elvisreales7@gmail.com>
2025-09-25 20:17:41 +0000

    ajustes en colores de cards (rigidos)

--- src/app/components/Timeline.tsx
+++ src/app/components/Timeline.tsx
@@ -13,9 +13,9 @@ type Props = {
 
 const Timeline: React.FC<Props> = ({ events }) => {
   return (
-    <ol className="relative border-s-4 border-[#1Ddad2]">
+    <ol className="relative border-s-4 border-[#00514B]">
       {events.map((event, index) => (
-        <li key={index} className="mb-10 ms-6">
+        <li key={index} className="mb-10 ms-10">
           <span className="absolute flex items-center justify-center w-8 h-8 rounded-full -start-4 bg-white ring-8 ring-[#1C2A29]">
             {event.icon ?? (
               <svg
@@ -27,12 +27,12 @@ const Timeline: React.FC<Props> = ({ events }) => {
               </svg>
             )}
           </span>
-	        <div className="p-4 bg-[#00514B] rounded-lg">
+	        <div className="p-4 shadow-[0_0px_18px_-4px_rgba(29,218,210,0.7)] rounded-lg">
             <div className="items-center justify-between mb-3 sm:flex">
               <time className="mb-1 text-xs font-normal text-gray-400 sm:order-last sm:mb-0">2 hours ago</time>
               <div className="text-sm font-normal text-gray-200 lex dark:text-gray-300">Thomas Lean commented on  <a href="#" className="font-semibold text-gray-600 dark:text-white hover:underline">{event.title}</a></div>
             </div>
-				    <div className="p-3 rounded-lg bg-[#1C2A29]">
+				    <div className="p-3 rounded-lg bg-[#00514B]">
               <p className=" text-xs italic font-normal text-gray-200">{event.description}</p>
             </div>
 		        </div>
