21c34a187674fe3d84d2d0767b0eccbb9a4967a0
realeselvis7 <elvisreales7@gmail.com>
2025-10-01 21:29:54 +0000

    Se agrega base para los 4 temas

--- src/app/components/ExperienceCard.tsx
+++ src/app/components/ExperienceCard.tsx
@@ -44,9 +44,9 @@ export default function ExperienceCard({
   }, [isMobile, initialHeight]);
 
   return (
-    <div className="experienceCard flex flex-col bg-[#1C2A29] rounded-b-2xl">
+    <div className="experienceCard flex flex-col bg-custom rounded-b-2xl">
       {/* Sección 1: Título + acciones */}
-      <div className="section1 flex items-center p-4 pb-8 bg-[#1C2A29] relative z-10 justify-between">
+      <div className="section1 flex items-center p-4 pb-8 bg-custom relative z-10 justify-between">
         <div className="flex gap-2  rounded">
           <h2 className="font-normal text-base text-[#00514B] leading-none">{title}</h2>
         </div>
